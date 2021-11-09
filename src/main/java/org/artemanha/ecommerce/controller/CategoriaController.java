74e39354a6ea106c2f08c381a4832c7bcedb2ef7
Camibaza <bazante_mila@hotmail.com>
2021-11-09 12:10:53 -0300

    @Camibaza [Criação do repository da categoria]

--- src/main/java/org/artemanha/ecommerce/controller/CategoriaController.java
+++ src/main/java/org/artemanha/ecommerce/controller/CategoriaController.java
@@ -1,6 +1,9 @@
 package org.artemanha.ecommerce.controller;
 
+import java.util.List;
+
 import org.artemanha.ecommerce.model.Categoria;
+import org.artemanha.ecommerce.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,55 +17,54 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 /**
- * Classe categoriaController
- * Esta Classe é responsável por controlar a entrada, saida, buscar, criação, atualização e delete de informações
+ * Classe categoriaController Esta Classe é responsável por controlar a entrada,
+ * saida, buscar, criação, atualização e delete de informações
  * 
  * @author MarceloNegromonte
- * @since 1.0
+ * @since 1.1
  * 
- *  * 
- * */
+ *        *
+ */
 
 @RestController
-@RequestMapping("/categoria") 
-@CrossOrigin("*") 
+@RequestMapping("/categoria")
+@CrossOrigin("*")
 public class CategoriaController {
 
-	@Autowired 
-	private CategoriaRepository repository; 
+	@Autowired
+	private CategoriaRepository repository;
 
-	@GetMapping 
-	public ResponseEntity<Categoria> GetAll(){
-		return ResponseEntity.ok(repository.finAll());
+	@GetMapping
+	public ResponseEntity<List<Categoria>> GetAll() {
+		return ResponseEntity.ok(repository.findAll());
 	}
-	
+
 	@GetMapping("/{id}")
-	public ResponseEntity<Categoria> GetById(@PathVariable long id){
+	public ResponseEntity<Categoria> getById(@PathVariable long id) {
 		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
 	}
 
 	@GetMapping("/artesanal/{artesanal}")
-	public ResponseEntity<Categoria> GetByArtesanal(@PathVariable String artesanal){
+	public ResponseEntity<List<Categoria>> getByArtesanal(@PathVariable String artesanal) {
 		return ResponseEntity.ok(repository.findAllByArtesanalContainingIgnoreCase(artesanal));
 	}
 
 	@GetMapping("/formapagamento/{formapagamento}")
-	public ResponseEntity<Categoria> GetByFormaPagamento(@PathVariable String formaPagamento){
-		return ResponseEntity.ok(repository.findAllByformaPagamentoContainingIgnoreCase(formaPagamento));
+	public ResponseEntity<List<Categoria>> getByFormaPagamento(@PathVariable String formaPagamento) {
+		return ResponseEntity.ok(repository.findAllByFormaPagamentoContainingIgnoreCase(formaPagamento));
 	}
 
 	@PostMapping
-	public ResponseEntity<Categoria> post(@RequestBody Categoria categoria){
+	public ResponseEntity<Categoria> post(@RequestBody Categoria categoria) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(categoria));
 	}
-	
+
 	@PutMapping
-	public ResponseEntity<Categoria> put(@RequestBody Categoria categoria){
+	public ResponseEntity<Categoria> put(@RequestBody Categoria categoria) {
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(categoria));
 	}
-	
+
 	@DeleteMapping
 	public void delete(@PathVariable long id) {
 		repository.deleteById(id);
