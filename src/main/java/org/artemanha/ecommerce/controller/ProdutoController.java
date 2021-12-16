4467faefe125090889d6e86c18e18ca68d1b1f03
Renomariocosme <renomario.cosme@gmail.com>
2021-12-16 10:12:30 -0300

    @Renomariocosme [atualizar]

--- src/main/java/org/artemanha/ecommerce/controller/ProdutoController.java
+++ src/main/java/org/artemanha/ecommerce/controller/ProdutoController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/produto")
-@CrossOrigin("*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProdutoController {
 	  
 	@Autowired
